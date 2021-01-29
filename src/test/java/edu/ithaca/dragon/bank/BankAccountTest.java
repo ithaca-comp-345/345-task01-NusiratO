3c5899e9be1eb98df113e40a977b03a459d887de
NusiratO <nolumegbon@ithaca.edu>
2021-01-28 21:47:27 -0500

    Added comments to my test

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -24,16 +24,36 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
+        /*
+        * The first 3 line equivalence class belongs to the nominal value
+        * however, I'm not entirely sure if that's correct because I'm still having 
+        * trouble understanding the concept of equvalence class and it's classes. I want
+        * to say it's not a border case for the first 3 but all the tests combine is a border case.
+        * however again, I'm not sure. Any equivalence cases or border cases I miss would be
+        * overly used special characters after the at symbol. 
+        */
         assertTrue(BankAccount.isEmailValid( "a@b.com"));
         assertTrue(BankAccount.isEmailValid("2@c.com"));
         assertTrue(BankAccount.isEmailValid("2@c.edu"));
+        /*
+        * it's equivalence class belongs to minimum
+        */
         assertFalse(BankAccount.isEmailValid(""));
+        /*
+        * It's equivalence class belongs to just above the minimum
+        */
         assertFalse(BankAccount.isEmailValid("a.com"));
         assertFalse(BankAccount.isEmailValid("a@b"));
         assertFalse(BankAccount.isEmailValid("abc"));
 
+        /*
+        * It's equivvalence class belongs to just below the maximum
+        */ 
         assertTrue(BankAccount.isEmailValid("a_b@c.com"));
         assertTrue(BankAccount.isEmailValid("a-b@c.com"));
+        /*
+        * It's equivvalence class belongs to Maximum
+        */ 
         assertTrue(BankAccount.isEmailValid("ab.de@c.com"));
         assertFalse(BankAccount.isEmailValid("ab-@c.com"));
         assertFalse(BankAccount.isEmailValid("a..b@c.com"));
